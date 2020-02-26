3248dc4d2b3e5b28a449c3f615a8dc6603ee57c5
slee233 <59944528+slee233@users.noreply.github.com>
2020-02-25 23:45:28 -0600

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,19 +1,3 @@
-jQuery(function($) {
-$('html').removeClass('nojs');
-$('html').addClass('hasjs');
-$('#email').on('focus', function(){
-	$('#input-email label').addClass('active');
-});
-$('#email').on('blur', function(){
-if($('#email').val().length == 0){
-	$('#input-email label').removeClass('active');
-}
-});
-/* $('#alt-box').on('click', function(){
-	$('#age-verification').prop('checked', true);
-	$('#alt-box').html("<p>&#10004;</p>");
-}); */
-});
 function a(){
 	confirm("Do you want to Sign Up?");
 }
